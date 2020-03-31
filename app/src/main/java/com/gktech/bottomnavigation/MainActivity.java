d57964a140cdabc3843b387701621d75fabe073a
alamin <alamingazi68@gmail.com>
2020-03-31 15:52:30 +0600

    Bottom Navigation with lifecycle

--- app/src/main/java/com/gktech/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/gktech/bottomnavigation/MainActivity.java
@@ -1,9 +1,12 @@
 package com.gktech.bottomnavigation;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("method","on Create");
         bottomNavigationView=findViewById(R.id.bottom_nav);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -40,5 +43,42 @@ public class MainActivity extends AppCompatActivity {
         TextView text= view.findViewById(R.id.badge_text);
         text.setText("12");
         itemView.addView(view);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("method","onstart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+
+        Log.d("method","on Resume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("method","on Pause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("method","onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("method","Restart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("method","onStop");
+        super.onStop();
     }
 }
